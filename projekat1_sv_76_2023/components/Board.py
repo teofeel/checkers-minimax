fc5967fcf7643092b3707aa41b6aa029c0e86fd6
teofeel <teodor.perun@gmail.com>
2024-05-17 16:12:19 +0200

    update

--- projekat1_sv_76_2023/components/Board.py
+++ projekat1_sv_76_2023/components/Board.py
@@ -53,9 +53,7 @@ class Board:
 
 
 
-    def check_draw_possible_move(self, row, column, player, player_pos):
-        player_pos_col, player_pos_row = player_pos
-
+    def check_draw_possible_move(self, row, column, player):
         if(not self.out_of_bounds(row,column) and self.board[row][column]==0):
             return row, column
         
@@ -63,19 +61,19 @@ class Board:
             ##### ovde greksa kad je kraljica ######
             if not player.is_queen:
                 row_temp = row+player.direction
-                if column-player_pos_col>0:
+                if column-player.column>0:
                     column_temp=column+abs(player.direction)
-                elif column-player_pos_col<0:
+                elif column-player.column<0:
                     column_temp=column-abs(player.direction)
             else:
-                if row>player_pos_row:
+                if row>player.row:
                     row_temp = row+1
                 else:
                     row_temp = row-1
 
-                if column-player_pos_col>0:
+                if column-player.column>0:
                     column_temp=column+1
-                elif column-player_pos_col<0:
+                elif column-player.column<0:
                     column_temp=column-1
 
             if(not self.out_of_bounds(row_temp,column_temp) and self.board[row_temp][column_temp]==0):
@@ -115,16 +113,16 @@ class Board:
             can_move_col1 = position_col+1
             can_move_col2 = position_col-1
             
-            can_move_row1, can_move_col1 = self.check_draw_possible_move(can_move_row, can_move_col1, self.board[position_row][position_col], (position_col, position_row))
-            can_move_row2, can_move_col2 = self.check_draw_possible_move(can_move_row, can_move_col2, self.board[position_row][position_col], (position_col, position_row))
+            can_move_row1, can_move_col1 = self.check_draw_possible_move(can_move_row, can_move_col1, self.board[position_row][position_col])
+            can_move_row2, can_move_col2 = self.check_draw_possible_move(can_move_row, can_move_col2, self.board[position_row][position_col])
 
             if self.board[position_row][position_col].is_queen:
                 can_move_row_queen = self.board[position_row][position_col].direction*(-1)+position_row
                 can_move_col1_queen = position_col+1
                 can_move_col2_queen = position_col-1
 
-                can_move_row3, can_move_col3 = self.check_draw_possible_move(can_move_row_queen, can_move_col1_queen, self.board[position_row][position_col], (position_col, position_row))
-                can_move_row4, can_move_col4 = self.check_draw_possible_move(can_move_row_queen, can_move_col2_queen, self.board[position_row][position_col], (position_col, position_row))
+                can_move_row3, can_move_col3 = self.check_draw_possible_move(can_move_row_queen, can_move_col1_queen, self.board[position_row][position_col])
+                can_move_row4, can_move_col4 = self.check_draw_possible_move(can_move_row_queen, can_move_col2_queen, self.board[position_row][position_col])
 
             #can_move_row1, can_move_row2 = self.check_draw_possible_mustattack(position_row, can_move_row1, can_move_row2)
             
@@ -150,16 +148,16 @@ class Board:
         else:
             return False
 
-    def remove_piece(self, player, move_col, move_row, piece_col, piece_row):
-        if move_row>piece_row:
+    def remove_piece(self, player, move_col, move_row):
+        if move_row>player.row:
             remove_piece_row = move_row-1
-            if move_col>piece_col:
+            if move_col>player.column:
                 remove_piece_col = move_col-1
             else:
                 remove_piece_col = move_col+1
         else:
-            remove_piece_row = piece_row-1
-            if move_col>piece_col:
+            remove_piece_row = player.row-1
+            if move_col>player.column:
                 remove_piece_col = move_col-1
             else:
                 remove_piece_col = move_col+1
@@ -184,7 +182,7 @@ class Board:
                and (possible_move_row==move_row and possible_move_col==move_col)):
                 
                 if abs(move_row-piece_row)==2:
-                    self.remove_piece(self.board[piece_row][piece_col], move_col, move_row, piece_col, piece_row)
+                    self.remove_piece(self.board[piece_row][piece_col], move_col, move_row)
                 
                 self.board[move_row][move_col] = self.board[piece_row][piece_col]
                 self.board[move_row][move_col].column = move_col
@@ -195,12 +193,10 @@ class Board:
                 self.board[move_row][move_col].draw_circle(window)
                 pygame.display.update()
 
-                ######## OVO PREGLEDATI ########
                 if(self.board[move_row][move_col].color == BLACK and move_row==0):
                     self.board[move_row][move_col].is_queen = True
                 elif(self.board[move_row][move_col].color == RED and move_row==ROWS-1):
                     self.board[move_row][move_col].is_queen = True
-                ################################
 
                 return True
             
